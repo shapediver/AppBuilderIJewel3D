39ce1dcc681a62c07db33a2ab19d4c4bd3329f7b
Michael Oppitz <michael@shapediver.com>
2025-11-20 12:54:03 +0100

    instancing implementation

--- src/webgi/utils/webGiProcessingUtils.ts
+++ src/webgi/utils/webGiProcessingUtils.ts
@@ -1,13 +1,31 @@
 import {ResOutputContent} from "@shapediver/sdk.geometry-api-sdk-v2";
-import {ISessionApi, SessionOutputData} from "@shapediver/viewer.session";
-import {CoreViewerApp, IModel, MathUtils, Mesh, Sphere} from "webgi";
+import {
+	ISessionApi,
+	ITreeNode,
+	SessionData,
+	SessionOutputData,
+} from "@shapediver/viewer.session";
+import {
+	CoreViewerApp,
+	IModel,
+	MathUtils,
+	Matrix4,
+	Mesh,
+	Object3D,
+	Sphere,
+} from "webgi";
 import {staticMaterialDatabase} from "./staticMaterialDatabase";
 
 const _models: Record<string, IModel[][]> = {};
-let _loadedOutputVersions: {[key: string]: string} = {};
+let _loadedOutputVersions: {
+	[key: string]: {
+		version: string;
+		models: IModel[];
+	};
+} = {};
 let _dynamicMaterialDatabase: {[key: string]: unknown} = {};
 let _loadedMaterialOutputVersion: string | undefined;
-let _initialFitToView = true;
+let _mainInstanceNode: Object3D | undefined;
 
 export const processMaterialDatabase = async (sessionApi: ISessionApi) => {
 	// first, search for the MaterialDatabase output and update the dynamicMaterialDatabase
@@ -48,47 +66,153 @@ export const processMaterialDatabase = async (sessionApi: ISessionApi) => {
 	}
 };
 
-export const processOutputs = async (
+export const processInstances = async (
 	viewport: CoreViewerApp | undefined,
 	sessionApi: ISessionApi,
 ) => {
-	const parameters = new URLSearchParams(window.location.search);
-	const zoomTo = parameters.get("webgiZoomTo");
+	if (!_mainInstanceNode) {
+		_mainInstanceNode = new Object3D();
+		_mainInstanceNode.name = "Instances";
+		viewport?.scene.modelRoot.add(_mainInstanceNode);
+	} else {
+		// clear the main instance node and remove all children
+		_mainInstanceNode.clear();
+		while (_mainInstanceNode.children.length > 0) {
+			// clear the data of the child nodes
+			(_mainInstanceNode.children[0] as any).dispose();
+			_mainInstanceNode.remove(_mainInstanceNode.children[0]);
+		}
+	}
+
+	// check all the children of the sessionApi.node for instances
+	for (const childNode of sessionApi.node.children) {
+		const instances: ITreeNode[] = [];
+		// traverse the child nodes to find instances
+		childNode.traverse((node) => {
+			for (const data of node.data)
+				if (data instanceof SessionData) instances.push(node);
+		});
+
+		// for each instance, we need to create a node for the instance
+		// and nodes for each transformation
+		// we need to convert the transformation matrix from gl-matrix to three.js matrix
+		// then we can call the processOutputs function to load the models
+		for (const instanceNode of instances) {
+			const instanceObject = new Object3D();
+			instanceObject.name =
+				instanceNode.originalName || instanceNode.name;
+
+			// for each child of the instance node, create a transformation node
+			for (const child of instanceNode.children) {
+				const transformationObject = new Object3D();
+				transformationObject.name = child.originalName || child.name;
+				// get the transformation matrix from gl-matrix format to three.js format
+				const matrices = child.transformations.map((t) => t.matrix);
+				// apply the transformation matrices to the transformation node
+				for (const matrix of matrices) {
+					const threeMatrix = new Matrix4();
+					threeMatrix.set(
+						matrix[0],
+						matrix[4],
+						matrix[8],
+						matrix[12],
+						matrix[1],
+						matrix[5],
+						matrix[9],
+						matrix[13],
+						matrix[2],
+						matrix[6],
+						matrix[10],
+						matrix[14],
+						matrix[3],
+						matrix[7],
+						matrix[11],
+						matrix[15],
+					);
+					transformationObject.applyMatrix4(threeMatrix);
+				}
+				instanceObject.add(transformationObject);
+
+				const outputs: {[outputId: string]: SessionOutputData} = {};
+				child.traverseData((data) => {
+					if (data instanceof SessionOutputData) {
+						const sessionOutputData = data as SessionOutputData;
+						const outputId = sessionOutputData.id;
+						if (outputId && !(outputId in outputs)) {
+							outputs[outputId] = sessionOutputData;
+						}
+					}
+				});
+
+				// each child of the transformation node needs to load the outputs
+				const loaded = await processOutputs(viewport, outputs, false);
+
+				// add all loaded models to the transformation object
+				for (const outputId in loaded) {
+					const models = loaded[outputId];
+					for (const model of models)
+						transformationObject.add(model.modelObject.clone());
+				}
+				instanceObject.add(transformationObject);
+			}
+			_mainInstanceNode.add(instanceObject);
+		}
+	}
+};
+
+export const processOutputs = async (
+	viewport: CoreViewerApp | undefined,
+	outputs: {[outputId: string]: SessionOutputData},
+	addToRoot: boolean = true,
+): Promise<{[outputId: string]: IModel[]}> => {
+	const loadedOutputs: {[outputId: string]: IModel[]} = {};
 
 	// iterate over all other outputs
-	for (const outputId in sessionApi.outputs) {
-		const outputApi = sessionApi.outputs[outputId];
+	for (const outputId in outputs) {
+		const outputData = outputs[outputId].responseOutput;
 
 		// if the output is already loaded, skip it
-		if (_loadedOutputVersions[outputId] === outputApi.version) continue;
+		if (_loadedOutputVersions[outputId]?.version === outputData.version) {
+			loadedOutputs[outputId] = _loadedOutputVersions[outputId].models;
+			continue;
+		}
 		// skip the MaterialDatabase output, this output is handled separately
 		if (
-			outputApi.name === "MaterialDatabase" ||
-			outputApi.displayname === "MaterialDatabase"
+			outputData.name === "MaterialDatabase" ||
+			outputData.displayname === "MaterialDatabase"
 		)
 			continue;
 
 		// iterate over all content in the output and load the glb content
-		const content = outputApi.content;
+		const content = outputData.content;
 		if (!content?.length) continue;
+
+		const models: IModel[] = [];
 		for (let i = 0; i < content.length; i++) {
 			const item = content[i];
 
 			switch (item.format) {
 				case "gltf":
-				case "glb":
-					await loadGlbContent(
+				case "glb": {
+					const model = await loadGlbContent(
 						viewport,
-						outputApi.name,
-						outputApi.uid,
+						outputData.name,
+						outputData.uid,
 						item,
 						i,
+						addToRoot,
 					);
+					if (model) models.push(model);
+				}
 			}
 		}
 
 		// store the version of the output
-		_loadedOutputVersions[outputId] = outputApi.version;
+		_loadedOutputVersions[outputId] = {
+			version: outputData.version,
+			models: models,
+		};
+		loadedOutputs[outputId] = models;
 	}
 
 	// scale the model to fit the viewport
@@ -104,10 +228,7 @@ export const processOutputs = async (
 		viewport.scene.setDirty();
 	}
 
-	if ((_initialFitToView && viewport) || (viewport && zoomTo === "true")) {
-		viewport.fitToView();
-		_initialFitToView = false;
-	}
+	return loadedOutputs;
 };
 
 /**
@@ -128,17 +249,30 @@ const loadGlbContent = async (
 	outputUid: string | undefined,
 	content: ResOutputContent,
 	index: number,
-): Promise<void> => {
+	addToRoot: boolean = true,
+): Promise<IModel | undefined> => {
 	if (!viewport) return;
 
 	// use the original uid if it exists, otherwise generate a new one
 	const uid = outputUid || MathUtils.generateUUID();
 
-	// load the model
-	const ms = await viewport.load(content.href + "#f" + index + ".glb", {
-		autoScale: false,
-		pseudoCenter: false,
-	});
+	let ms: IModel | undefined;
+	if (addToRoot) {
+		// load the model
+		ms = await viewport.load(content.href + "#f" + index + ".glb", {
+			autoScale: false,
+			pseudoCenter: false,
+		});
+	} else {
+		ms = (await viewport
+			.getManager()!
+			.importer!.importSingle(content.href + "#f" + index + ".glb", {
+				autoScale: false,
+				pseudoCenter: false,
+				waitForFullLoad: true,
+			})) as unknown as IModel;
+	}
+	if (!ms) return;
 	ms.name = outputName;
 
 	// dispose the old model
@@ -151,8 +285,10 @@ const loadGlbContent = async (
 	if (!_models[uid]) _models[uid] = [];
 	_models[uid][index] = [ms];
 
-	// // apply the material
+	// apply the material
 	await applyMaterial(viewport, ms);
+
+	return ms;
 };
 
 const applyMaterial = async (viewport: CoreViewerApp, ms: IModel) => {
